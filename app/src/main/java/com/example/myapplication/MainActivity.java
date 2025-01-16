0b342a54895ed70882791ca5327dbed9e0ae22b5
himel <abirhimel90@gmail.com>
2025-01-17 01:11:45 +0600

    step2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
-       private void init(){
+    private void init(){
 
         btn1=findViewById(R.id.btn1);
         btn2=findViewById(R.id.btn2);
@@ -38,47 +38,169 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-       public void Check(View view) {
+    public void Check(View view) {
         Button btnCurrent=(Button) view;
         count++;
-        if(flag == 0){
-           btnCurrent.setText("X");
-           flag=1;
-         }else {
-            btnCurrent.setText("O");
-            flag=0;
+        if (btnCurrent.getText().toString().equals("")) {
+
+
+            if (flag == 0) {
+                btnCurrent.setText("X");
+                flag = 1;
+            } else {
+                btnCurrent.setText("O");
+                flag = 0;
+            }
+            if (count > 4) {
+
+                b1 = btn1.getText().toString();
+                b2 = btn2.getText().toString();
+                b3 = btn3.getText().toString();
+                b4 = btn4.getText().toString();
+                b5 = btn5.getText().toString();
+                b6 = btn6.getText().toString();
+                b7 = btn7.getText().toString();
+                b8 = btn8.getText().toString();
+                b9 = btn9.getText().toString();
+
+                if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")) {
+                    //1
+                    Toast.makeText(this, "Winner is " + b1, Toast.LENGTH_SHORT).show();
+                    btn1.setText("");
+                    btn2.setText("");
+                    btn3.setText("");
+                    btn4.setText("");
+                    btn5.setText("");
+                    btn6.setText("");
+                    btn7.setText("");
+                    btn8.setText("");
+                    btn9.setText("");
+                    count=0;
+                    flag=0;
+
+
+                } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
+                    //2
+                    Toast.makeText(this, "Winner is " + b4, Toast.LENGTH_SHORT).show();
+
+                    btn1.setText("");
+                    btn2.setText("");
+                    btn3.setText("");
+                    btn4.setText("");
+                    btn5.setText("");
+                    btn6.setText("");
+                    btn7.setText("");
+                    btn8.setText("");
+                    btn9.setText("");
+                    count=0;
+                    flag=0;
+
+                } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
+                    //3
+                    Toast.makeText(this, "Winner is " + b7, Toast.LENGTH_SHORT).show();
+                    btn1.setText("");
+                    btn2.setText("");
+                    btn3.setText("");
+                    btn4.setText("");
+                    btn5.setText("");
+                    btn6.setText("");
+                    btn7.setText("");
+                    btn8.setText("");
+                    btn9.setText("");
+                    count=0;
+                    flag=0;
+
+                } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
+                    //4
+                    Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
+                    btn1.setText("");
+                    btn2.setText("");
+                    btn3.setText("");
+                    btn4.setText("");
+                    btn5.setText("");
+                    btn6.setText("");
+                    btn7.setText("");
+                    btn8.setText("");
+                    btn9.setText("");
+                    count=0;
+                    flag=0;
+
+
+
+                } else if (b2.equals(b5) && b5.equals(b8) && !b8.equals("")) {
+                    //5
+                    Toast.makeText(this, "winner is " + b2, Toast.LENGTH_SHORT).show();
+
+                    btn1.setText("");
+                    btn2.setText("");
+                    btn3.setText("");
+                    btn4.setText("");
+                    btn5.setText("");
+                    btn6.setText("");
+                    btn7.setText("");
+                    btn8.setText("");
+                    btn9.setText("");
+                    count=0;
+                    flag=0;
+
+                } else if (b3.equals(b6) && b6.equals(b9) && !b9.equals("")) {
+                    //6
+                    Toast.makeText(this, "winner is " + b3, Toast.LENGTH_SHORT).show();
+
+                    btn1.setText("");
+                    btn2.setText("");
+                    btn3.setText("");
+                    btn4.setText("");
+                    btn5.setText("");
+                    btn6.setText("");
+                    btn7.setText("");
+                    btn8.setText("");
+                    btn9.setText("");
+                    count=0;
+                    flag=0;
+
+                } else if (b1.equals(b5) && b5.equals(b9) && !b9.equals("")) {
+                    //7
+                    Toast.makeText(this, "winner is " + b1, Toast.LENGTH_SHORT).show();
+
+                    btn1.setText("");
+                    btn2.setText("");
+                    btn3.setText("");
+                    btn4.setText("");
+                    btn5.setText("");
+                    btn6.setText("");
+                    btn7.setText("");
+                    btn8.setText("");
+                    btn9.setText("");
+                    count=0;
+                    flag=0;
+
+                } else if (b3.equals(b5) && b5.equals(b7) && !b7.equals("")) {
+                    //8
+                    Toast.makeText(this, "winner is " + b3, Toast.LENGTH_SHORT).show();
+
+                    btn1.setText("");
+                    btn2.setText("");
+                    btn3.setText("");
+                    btn4.setText("");
+                    btn5.setText("");
+                    btn6.setText("");
+                    btn7.setText("");
+                    btn8.setText("");
+                    btn9.setText("");
+                    count=0;
+                    flag=0;
+
+                }
+
+
+            }
+
+
         }
-        if (count>4){
-
-            b1=btn1.getText().toString();
-            b2=btn2.getText().toString();
-            b3=btn3.getText().toString();
-            b4=btn4.getText().toString();
-            b5=btn5.getText().toString();
-            b6=btn6.getText().toString();
-            b7=btn7.getText().toString();
-            b8=btn8.getText().toString();
-            b9=btn9.getText().toString();
-
-           if(b1.equals(b2) && b2.equals(b3) && !b1.equals("")){
-           //1
-               Toast.makeText(this,"Winner is " + b1 ,Toast.LENGTH_SHORT).show();
-           } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
-              //2
-               Toast.makeText(this,"Winner is " + b4,Toast.LENGTH_SHORT).show();
-           } else if (b7.equals(b8)&& b8.equals(b9)&& !b7.equals("")) {
-               //3
-               Toast.makeText(this,"Winner is " + b7,Toast.LENGTH_SHORT).show();
-           }
 
 
-        }
-
-
-
-
-
-     }
+    }
 
 
 
